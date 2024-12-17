70261efa9a92dbdc3da87de6870d3554cff42781
shu <7taizai1020@gmail.com>
2024-12-18 07:13:59 +0900

    Add fv section css

---
+++ assets/js/swiper.js
@@ -0,0 +1,9 @@
+// const swiper = new Swiper(".p-fv__slide", {
+//   loop: true,
+//   speed: 3000,
+//   effect: "fade",
+//   autoplay: {
+//     delay: 2000,
+//     disableOnInteraction: false,
+//   }
+// })
